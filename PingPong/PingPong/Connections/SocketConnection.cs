d0185a4810dcdbc477eb95e0cd51485a6ef52b90
citizen6 <theLittleCitizen6@gmail.com>
2020-09-02 21:49:22 +0300

    added abstrtaction and implements ecsept request handler

---
+++ PingPong/PingPong/Connections/SocketConnection.cs
@@ -0,0 +1,62 @@
+﻿using PingPong.Server.RequestHendlers;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace PingPong.Server.Connections
+{
+    public class SocketConnection: IConnection
+    {
+        private bool _isRunning;
+        private Socket _socket;
+        private IRequestHandler _requestHandler;
+        public SocketConnection(Socket socket, IRequestHandler requestHandler)
+        {
+            _socket = socket;
+            _requestHandler = requestHandler;
+
+        }
+
+        public bool IsRunning => _socket.Connected;
+
+        public void Dispose()
+        {
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Close();
+        }
+
+        public void Run()
+        {
+            // Incoming data from the client.    
+            string data = null;
+            byte[] bytes = null;
+
+            while (true)
+            {
+                bytes = new byte[1024];
+                int bytesRec = _socket.Receive(bytes);
+                //data += Encoding.ASCII.GetString(bytes, 0, bytesRec); 
+                data = Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                if (data != null)
+                {
+                    Console.WriteLine(data);
+                    if (data == "exit")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        _requestHandler.Handle(data);
+                    }
+                }
+            }
+
+            Console.WriteLine("Text received : {0}", data);
+
+            byte[] msg = Encoding.ASCII.GetBytes(data); // the implement of the request handler
+            _socket.Send(msg);
+        }
+    }
+    }
+}
---
+++ PingPong/PingPong/Listener/SocketListener.cs
@@ -0,0 +1,75 @@
+﻿using PingPong.Server.Connections;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingPong.Server.Listener
+{
+    public class SocketListener : IListener
+    {
+        const int MAX_CONNECTIONS = 10;
+        private List<IConnection> _connections;
+        private IPHostEntry _host;
+        private IPAddress _ipAddress;
+        private IPEndPoint _localEndPoint;
+        public SocketListener() 
+        {
+            // Get Host IP Address that is used to establish a connection  
+            // In this case, we get one IP address of localhost that is IP : 127.0.0.1  
+            // If a host has multiple addresses, you will get a list of addresses  
+            _host = Dns.GetHostEntry("localhost");
+            _ipAddress = _host.AddressList[0];
+            _localEndPoint = new IPEndPoint(_ipAddress, 11000);
+            _connections = new List<IConnection>();
+            
+        }
+
+        public void StartServer()
+        {
+            try
+            {
+                Socket listener = new Socket(_ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                listener.Bind(_localEndPoint);
+                listener.Listen(MAX_CONNECTIONS);
+
+                while (true)
+                {
+                    Socket handler = listener.Accept();
+                    Task Connect = new Task(() =>
+                    {
+                        _connections[FindFirstIndexAvailable()] = new SocketConnection(handler);
+                        _connections[FindFirstIndexAvailable()].Run();
+                        _connections[FindFirstIndexAvailable()].Dispose();
+                    });
+                    Connect.Start();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\n Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        private int FindFirstIndexAvailable()
+        {
+            while (true)
+            {
+                int i = 0;
+                while(i<MAX_CONNECTIONS)
+                {
+                    if(!_connections[i].IsRunning)
+                    {
+                        return i;
+                    }
+                    i++;
+                }
+            }
+        }
+    }
+}
